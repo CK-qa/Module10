dcde5dd8144e3da5708ef204733b368cbfb762d8
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-12-07 18:52:24 +0300

    commit

--- src/main/ClassJ.java
+++ src/main/ClassJ.java
@@ -1,11 +1,34 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ClassJ {
 
-    public void foo() {
-        String s = "Stststs";
-        s.subs
+    public static void foo(String string1, int integer2) {
+        MyString reverseStr = (str) -> {
+            String result = "";
+
+            for(int i = str.length()-1; i >= 0; i--)
+                result += str.charAt(i);
+
+            return result;
+        };
     }
     //test
 
+    interface MyString {
+        String reverseStr(String str);
+    }
+
+    void test(Consumer<String> foo) {
+
+    }
+
+    public void testHint() {
+        test(test -> {
+            System.out.println(test);
+        });
+    }
 }
